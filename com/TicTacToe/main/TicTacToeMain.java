2a69e2d51a03a8880d4a9932f7f4a8ffaba5ba82
PranjalChambial1998 <61262507+PranjalChambial1998@users.noreply.github.com>
2020-02-20 23:01:02 +0530

    commit 3 possibly final.

--- com/TicTacToe/main/TicTacToeMain.java
+++ com/TicTacToe/main/TicTacToeMain.java
@@ -7,6 +7,7 @@ public class TicTacToeMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TTT.createBoard();
+		System.out.println("\nBoard is :\n");
 		TTT.printBoard(true);
 	}
 
